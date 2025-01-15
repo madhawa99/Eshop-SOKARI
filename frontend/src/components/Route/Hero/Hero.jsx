d27e18b4b0a20f96bf3c1b8a7216b89b7c40e8f5
Madhawa Chamuditha <50737657+madhawa99@users.noreply.github.com>
2025-01-15 13:20:17 +0530

    Home page update

--- frontend/src/components/Route/Hero/Hero.jsx
+++ frontend/src/components/Route/Hero/Hero.jsx
@@ -8,14 +8,14 @@ const Hero = () => {
       className={`relative min-h-[70vh] 800px:min-h-[80vh] w-full bg-no-repeat ${styles.noramlFlex}`}
       style={{
         backgroundImage:
-          "url(https://themes.rslahmed.dev/rafcart/assets/images/banner-2.jpg)",
+          "url(https://static01.nyt.com/images/2019/02/03/travel/03frugal-srilanka01/03frugal-srilanka01-videoSixteenByNineJumbo1600.jpg)"
       }}
     >
       <div className={`${styles.section} w-[90%] 800px:w-[60%]`}>
         <h1
-          className={`text-[35px] leading-[1.2] 800px:text-[60px] text-[#3d3a3a] font-[600] capitalize`}
+          className={`text-[35px] leading-[1.2] 800px:text-[60px] text-[#ffffff] font-[600] capitalize`}
         >
-          Best Collection for <br /> home Decoration
+          Explore Our Categories 
         </h1>
         <p className="pt-5 text-[16px] font-[Poppins] font-[400] text-[#000000ba]">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae,
