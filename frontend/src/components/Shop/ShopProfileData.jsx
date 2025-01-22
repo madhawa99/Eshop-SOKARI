7d85bdc37a0310c65a52e65add3c52ebff2073fd
Madhawa Chamuditha <50737657+madhawa99@users.noreply.github.com>
2025-01-23 01:47:22 +0530

    update 2

--- frontend/src/components/Shop/ShopProfileData.jsx
+++ frontend/src/components/Shop/ShopProfileData.jsx
@@ -30,7 +30,7 @@ const ShopProfileData = ({ isOwner }) => {
           <div className="flex items-center" onClick={() => setActive(1)}>
             <h5
               className={`font-[600] text-[20px] ${
-                active === 1 ? "text-red-500" : "text-[#333]"
+                active === 1 ? "text-cyan-500" : "text-white"
               } cursor-pointer pr-[20px]`}
             >
               Shop Products
@@ -39,7 +39,7 @@ const ShopProfileData = ({ isOwner }) => {
           <div className="flex items-center" onClick={() => setActive(2)}>
             <h5
               className={`font-[600] text-[20px] ${
-                active === 2 ? "text-red-500" : "text-[#333]"
+                active === 2 ? "text-cyan-500" : "text-white"
               } cursor-pointer pr-[20px]`}
             >
               Running Events
@@ -49,7 +49,7 @@ const ShopProfileData = ({ isOwner }) => {
           <div className="flex items-center" onClick={() => setActive(3)}>
             <h5
               className={`font-[600] text-[20px] ${
-                active === 3 ? "text-red-500" : "text-[#333]"
+                active === 3 ? "text-cyan-500" : "text-white"
               } cursor-pointer pr-[20px]`}
             >
               Shop Reviews
@@ -60,7 +60,7 @@ const ShopProfileData = ({ isOwner }) => {
           {isOwner && (
             <div>
               <Link to="/dashboard">
-                <div className={`${styles.button} !rounded-[4px] h-[42px]`}>
+                <div className={`${styles.button} !rounded-[4px] h-[42px] bg-cyan-400`}>
                   <span className="text-[#fff]">Go Dashboard</span>
                 </div>
               </Link>
@@ -93,7 +93,7 @@ const ShopProfileData = ({ isOwner }) => {
               ))}
           </div>
           {events && events.length === 0 && (
-            <h5 className="w-full text-center py-5 text-[18px]">
+            <h5 className="w-full text-center py-5 text-[18px] text-white">
               No Events have for this shop!
             </h5>
           )}
@@ -112,10 +112,10 @@ const ShopProfileData = ({ isOwner }) => {
                 />
                 <div className="pl-2">
                   <div className="flex w-full items-center">
-                    <h1 className="font-[600] pr-2">{item.user.name}</h1>
+                    <h1 className="font-[600] pr-2 text-white">{item.user.name}</h1>
                     <Ratings rating={item.rating} />
                   </div>
-                  <p className="font-[400] text-[#000000a7]">{item?.comment}</p>
+                  <p className="font-[400] text-gray-400">{item?.comment}</p>
                   <p className="text-[#000000a7] text-[14px]">{"2days ago"}</p>
                 </div>
               </div>
