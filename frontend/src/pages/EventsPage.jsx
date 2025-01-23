0886ff67a26625b0dfd896f316275dc40554b32f
Madhawa Chamuditha <50737657+madhawa99@users.noreply.github.com>
2025-01-23 11:52:23 +0530

    event page

--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 import EventCard from "../components/Events/EventCard";
 import Header from "../components/Layout/Header";
 import Loader from "../components/Layout/Loader";
-import Footer from "../components/Layout/Footer"; 
+import Footer from "../components/Layout/Footer";
 
 const EventsPage = () => {
   const { allEvents, isLoading } = useSelector((state) => state.events);
@@ -14,7 +14,9 @@ const EventsPage = () => {
       ) : (
         <div>
           <Header activeHeading={4} />
-          <EventCard active={true} data={allEvents && allEvents[0]} />
+          {allEvents && allEvents.map((event, index) => (
+            <EventCard key={index} active={true} data={event} />
+          ))}
           <Footer />
         </div>
       )}
