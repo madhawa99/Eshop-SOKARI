21593f4fdfdda4a6988f8c81d822905058da186d
Madhawa Chamuditha <50737657+madhawa99@users.noreply.github.com>
2025-01-23 21:48:54 +0530

    Update Cart.jsx

--- frontend/src/components/cart/Cart.jsx
+++ frontend/src/components/cart/Cart.jsx
@@ -37,7 +37,7 @@ const Cart = ({ setOpenCart }) => {
                 onClick={() => setOpenCart(false)}
               />
             </div>
-            <h5>Cart Items is empty!</h5>
+            <h5 className="text-white">Cart Items is empty!</h5>
           </div>
         ) : (
           <>
